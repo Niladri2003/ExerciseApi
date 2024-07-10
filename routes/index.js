e6ca43ca006c8ae6f1852b21ff06461cb1353c68
sumanpanja608 <sumanpanja608@gmail.com>
2024-07-10 09:52:53 +0530

    changes github

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Exercise Free Api' });
+  res.render('index', { title: 'Exercise Free Api edited by' });
 });
 
 module.exports = router;
